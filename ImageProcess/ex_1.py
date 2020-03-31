af3680290de2caa8e3e6806d92f7f0e60f0ac79c
Ayush-Ranjan <53467641+Ayush-Ranjan@users.noreply.github.com>
2020-04-01 02:13:30 +0530

    Update ex_1.py

--- ImageProcess/ex_1.py
+++ ImageProcess/ex_1.py
@@ -1,6 +1,12 @@
 from PIL import Image
 from numpy import *
+from pylab import *
 from scipy.ndimage import filters
 if __name__=='__main__':
-    im=array(Image.open("robot.jpg").convert("L"))
-    im2=filters.gaussian_filter(im,5)
+    im=array(Image.open("robot.jpg"))
+    im2=zeros(im.shape)
+    for i in range(3):
+        im2[:,:,i]=filters.gaussian_filter(im[:,:,i],5)
+        im2=uint8(im2)
+    imshow(im)
+    show()
